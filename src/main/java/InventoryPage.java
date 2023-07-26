a78ce1d8007ed9bfda51f4556e6ff49a5e0a7d52
Milos <milosmilkic445@gmail.com>
2023-07-26 21:01:21 +0200

    AddSwagTest

---
+++ src/main/java/InventoryPage.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class InventoryPage extends BasePage {
+    @FindBy(id = "add-to-cart-sauce-labs-fleece-jacket")
+    WebElement productJacket;
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    WebElement productBackpack;
+
+    @FindBy(id = "add-to-cart-sauce-labs-onesie")
+    WebElement productOnesie;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    WebElement productBikeLight;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bolt-t-shirt")
+    WebElement productT_Shirt;
+
+    @FindBy(className = "product_sort_container")
+    WebElement sortContainer;
+
+    @FindBy(css = "#shopping_cart_container .shopping_cart_link ")
+    WebElement shoppingCart;
+
+    @FindBy(css = ".header_secondary_container .title")
+    WebElement pageOfProducts;
+
+    public InventoryPage(ChromeDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void addJacket()
+    {
+        productJacket.click();
+    }
+    public void addBackpack()
+    {
+        productBackpack.click();
+    }
+    public void addOnesie()
+    {
+        productOnesie.click();
+    }
+    public void addT_Shirt()
+    {
+        productT_Shirt.click();
+    }
+    public void addBikeLight()
+    {
+        productBikeLight.click();
+    }
+
+    public void sortProducts()
+    {
+        Select select = new Select(sortContainer);
+        select.selectByValue("lohi");
+    }
+
+    public String getCartNumber()
+    {
+        return  shoppingCart.getText();
+    }
+    public void clickCart()
+    {
+        shoppingCart.click();
+    }
+
+    public String productsPage()
+    {
+        return pageOfProducts.getText();
+    }
+}
---
+++ src/test/java/SwagLabsTest.java
@@ -0,0 +1,86 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SwagLabsTest extends BaseTest {
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
+    CartPage cartPage;
+    CheckoutStepOnePage checkoutStepOnePage;
+    CheckoutStepTwoPage checkoutStepTwoPage;
+
+
+    @BeforeMethod
+    public void setUp()
+    {
+        driver = openBrowser();
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutStepOnePage = new CheckoutStepOnePage(driver);
+        checkoutStepTwoPage = new CheckoutStepTwoPage(driver);
+    }
+
+    @Test
+    public void sortAndAddToCartThreeProducts()
+    {
+        loginPage.loginOnPage("standard_user", "secret_sauce");
+        inventoryPage.sortProducts();
+        inventoryPage.addOnesie();
+        inventoryPage.addBikeLight();
+        inventoryPage.addT_Shirt();
+        Assert.assertEquals(inventoryPage.getCartNumber(), "3");
+    }
+
+    @Test
+    public void addToCartTwoProductsAndRemoveFromInsideCart()
+    {
+        loginPage.loginOnPage("standard_user", "secret_sauce");
+        inventoryPage.addBackpack();
+        inventoryPage.addJacket();
+        inventoryPage.clickCart();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/cart.html");
+
+        cartPage.removeBackpack();
+        cartPage.removeJacket();
+        cartPage.continueShopping();
+        Assert.assertEquals(inventoryPage.productsPage(), "Products");
+        Assert.assertEquals(inventoryPage.getCartNumber(), "");
+    }
+
+
+    @Test
+    public void verifyTotalAndItemTotalPrice()
+    {
+        loginPage.loginOnPage("standard_user", "secret_sauce");
+        inventoryPage.addBackpack();
+        inventoryPage.addBikeLight();
+        inventoryPage.clickCart();
+        cartPage.clickCheckout();
+        checkoutStepOnePage.inputForm("Miki", "Misko", "11000");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/checkout-step-two.html");
+        Assert.assertEquals(checkoutStepTwoPage.getItemTotalPrice(), "Item total: $39.98");
+        Assert.assertEquals(checkoutStepTwoPage.getTotalPrice(), "Total: $43.18");
+    }
+
+    @Test
+    public void buyProduct()
+    {
+        loginPage.loginOnPage("standard_user", "secret_sauce");
+        inventoryPage.addJacket();
+        inventoryPage.clickCart();
+        cartPage.clickCheckout();
+        checkoutStepOnePage.inputForm("Miki", "Misko", "11000");
+        checkoutStepTwoPage.clickFinish();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/checkout-complete.html");
+        Assert.assertEquals(checkoutStepTwoPage.getInfoMessage(),"Thank you for your order!");
+    }
+
+    @AfterMethod
+    public void after()
+    {
+        driver.quit();
+    }
+}
+
